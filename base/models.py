7feab77a51b0ef9c2150d8f0e2d8df096cba92b5
Arunava <arunavaskar@outlook.com>
2021-03-18 19:44:30 +0530

    models and task list view

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 # Create your models here.
+
+#model classes are the tables
+#atributes are the model columns
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null= True, blank= True)
+    title = models.CharField(max_length= 200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default= False)
+    created = models.DateTimeField(auto_now_add= True) 
+
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['complete']
