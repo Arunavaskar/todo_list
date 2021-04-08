37ce7094f865939cf289fdbce8e63f15ca156b2c
Arunava <arunavaskar@outlook.com>
2021-04-08 19:44:08 +0530

    1. RegisterPage 2. redirect_authenticated_user = True in LoginView

--- base/views.py
+++ base/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView #update view modifies the data
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView #update view modifies the data
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Task
 # Create your views here.
@@ -15,12 +16,28 @@ class CustomLoginView(LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
     # if the user is authenticated, that user won't be allowed in the log in page
-    redirect_authenticated_user = False
+    redirect_authenticated_user = True
 
     # when the user logs in the user will be redirected to the tasks page showing the tasks lists
     def get_success_url(self):
         return reverse_lazy('tasks')
 
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 
 class TaskList(LoginRequiredMixin, ListView):
